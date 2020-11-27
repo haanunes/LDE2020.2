7391c9ca9d3f21d55acf74f82b06bc7bb5da7ba5
Hélder Nunes <helder.nunes@afogados.ifpe.edu.br>
2020-11-27 16:12:47 -0300

    Primeiro teste com todas as inserções.

--- src/LDE.java
+++ src/LDE.java
@@ -3,37 +3,67 @@ public class LDE {
 
     public Produto cabeca;
     public Produto cauda;
-    public int tamanho=0;
+    public int tamanho = 0;
 
-    public void inserirInicio(Produto novo) {
+    private void inserirInicio(Produto novo) {
         if (cabeca == null) {// ou this.tamanho==0
             cabeca = novo;
             cauda = novo;
             tamanho++;
-        }
-        else{
-            novo.proximo=cabeca;
-            cabeca.anterior=novo;
-            cabeca=novo;
+        } else {
+            novo.proximo = cabeca;
+            cabeca.anterior = novo;
+            cabeca = novo;
             tamanho++;
         }
     }
-    
-    public void inserirFinal(Produto novo){
-         if (cabeca == null) {// ou this.tamanho==0
+
+    private void inserirFinal(Produto novo) {
+        if (cabeca == null) {// ou this.tamanho==0
             cabeca = novo;
             cauda = novo;
             tamanho++;
+        } else {
+            cauda.proximo = novo;
+            novo.anterior = cauda;
+            cauda = novo;
+            tamanho++;
         }
-         else {
-             cauda.proximo=novo;
-             novo.anterior=cauda;
-             cauda=novo;
-             tamanho++;
-         }
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void inserir(Produto novo, int indice) {
+        if (tamanho == 0 || indice <= 0) {
+            inserirInicio(novo);
+        } else if (indice >= tamanho) {
+            inserirFinal(novo);
+        } else {
+            Produto aux = cabeca;
+            //exemplo indice = 3
+            for (int i = 0; i < indice - 1; i++) {
+                aux = aux.proximo;
+            }
+            // achei a posição correta do aux. ele parou um indice antes.
+            (aux.proximo).anterior = novo;
+            novo.proximo = aux.proximo;
+            //a troca a seguir ela não pode ser executada antes da anterior
+            //pois iria perder a ligação com o elemento que está na posição
+            //indiceQueQuerAdicionar+1
+            aux.proximo = novo;
+            novo.anterior = aux;
+            tamanho++;
+        }
     }
     
-    public int getTamanho(){
-        return tamanho;
+    public void imprimir(){
+        Produto aux=cabeca;
+        for (int i =0; i<tamanho;i++){
+            System.out.print("| "+aux.descricao+" R$ "+aux.preco+" |");
+            aux=aux.proximo;
+        }
+        System.out.println("");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 
 public class Main {
     public static void main (String [] args){
+        LDE lista = new LDE();
+        lista.inserir(new Produto("Coca-cola 2L", 6.0),0);
+        lista.inserir(new Produto("Açucar 1 Kg", 4.0),0);
+        lista.inserir(new Produto("Feijão 2 Kg", 5.0),1);
+        
+        lista.imprimir();
+        
     }
 }
