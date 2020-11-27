69383db06b3a34b58859ad474c5b346a3e8ec3c9
Hélder Nunes <helder.nunes@afogados.ifpe.edu.br>
2020-11-27 16:12:14 -0300

    Método construtor

--- src/Produto.java
+++ src/Produto.java
@@ -3,4 +3,10 @@ public class Produto {
     public String descricao;
     public double preco;
     public Produto anterior, proximo;
+    
+    public Produto(String descricao, double p){
+        System.out.println("Construindo um produto...");
+        this.descricao=descricao;
+        preco=p;
+    }
 }
