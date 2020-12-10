50b72d351733993c85cb0e1b31a4b88a152f795b
Hélder Nunes <helder.nunes@afogados.ifpe.edu.br>
2020-12-10 11:45:11 -0300

    Primeiro teste com todas as inserções.

--- src/LDE.java
+++ src/LDE.java
@@ -55,6 +55,16 @@ public class LDE {
             aux.proximo = novo;
             novo.anterior = aux;
             tamanho++;
+            /*
+            //exemplo da revisão com coca cola do dia 04/12/2020 parte 1
+            // desvantagem desse método é: utilizar mais uma variável para ocupar RAM
+            Produto coca = aux.proximo;
+            aux.proximo=novo;
+            novo.anterior=aux;
+            coca.anterior=novo;
+            novo.proximo=coca;
+            tamanho++;
+*/
         }
     }
     
@@ -65,5 +75,96 @@ public class LDE {
             aux=aux.proximo;
         }
         System.out.println("");
+    }
+    
+    public void imprimirIniciandoNaCauda(){
+        Produto aux=cauda;
+        for (int i =0; i<tamanho;i++){
+            System.out.print("| "+aux.descricao+" R$ "+aux.preco+" |");
+            aux=aux.anterior;
+        }
+        System.out.println("");
+    }
+    
+    public Produto removerInicio(){
+        if (tamanho==0){
+            System.out.println("Lista vazia!");
+            return null;
+        }
+        else if (tamanho ==1){
+            Produto removido = cabeca;
+            cabeca= null;
+            cauda = null;
+            tamanho--;
+            return removido;
+        }
+        else{
+            Produto removido = cabeca;
+            cabeca=cabeca.proximo;
+            cabeca.anterior.proximo=null;
+            cabeca.anterior=null;
+            tamanho--;
+            return removido;
+        }
+    }
+    
+    public Produto removerFinal(){
+         if (tamanho==0){
+            System.out.println("Lista vazia!");
+            return null;
+        }
+        else if (tamanho ==1){
+            Produto removido = cabeca;
+            cabeca= null;
+            cauda = null;
+            tamanho--;
+            return removido;
+        }
+        else{
+            Produto u = cauda; //u de ultimo
+            // maneira ruim, pois gasta mais uma variável
+            /*
+            Produto p = u.anterior;  // p de penultimo
+            cauda = p;
+            u.anterior = null;
+            p.proximo=null;
+            tamanho --;
+            return u;
+            */
+            cauda = u.anterior;
+            cauda.proximo.anterior=null; //ou ....  u.anterior=null
+            cauda.proximo=null;
+            tamanho --;
+            return u;
+        }
+    }
+    
+    public Produto remover(int i){
+        if (i<=0){
+           return removerInicio();
+        }
+        else if (i>= tamanho-1){
+            return removerFinal();
+        }
+        else {
+            Produto aux = cabeca;
+            for (int j =0; j<i-1 ; j++){
+                aux = aux.proximo;
+            }
+            Produto removido = aux.proximo;
+            aux.proximo=removido.proximo;
+            aux.proximo.anterior=aux;
+            removido.proximo=null;
+            removido.anterior=null;
+            tamanho--;
+            return removido;
+        }
+    }
+    public void inserirOrdenadoPeloPreco(Produto novo){
+        //tarefinha de casa
+    }
+    
+    public void inserirMelhorComplexidade(Produto novo){
+        // para casa
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
 
 public class Main {
-    public static void main (String [] args){
+
+    public static void main(String[] args) {
         LDE lista = new LDE();
-        lista.inserir(new Produto("Coca-cola 2L", 6.0),0);
-        lista.inserir(new Produto("Açucar 1 Kg", 4.0),0);
-        lista.inserir(new Produto("Feijão 2 Kg", 5.0),1);
-        
+        lista.inserir(new Produto("Coca-cola 2L", 6.0), 0);
+        lista.inserir(new Produto("Açucar 1 Kg", 4.0), 0);
+        lista.inserir(new Produto("Feijão 2 Kg", 5.0), 1);
+        lista.inserir(new Produto("Arroz 1 kg", 4.0), 1);
+        lista.inserir(new Produto("Cerveja Itaipava lata", 2.5), 1);
+        lista.inserir(new Produto("Doce de leite", 5), lista.getTamanho());
         lista.imprimir();
         
+        lista.removerInicio();
+        System.out.println("Impressão depois da remoção inicio /n");
+        lista.imprimir();
+        lista.removerFinal();
+        System.out.println("Impressão depois da remoção final /n");
+        lista.imprimir();
+        lista.remover(1);
+        System.out.println("Impressão depois da remoção indice 1 /n");
+        lista.imprimir();
+        System.out.println("Impressão inversa");
+        lista.imprimirIniciandoNaCauda();
+
     }
 }
